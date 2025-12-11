8a8218acd0b6e98310b62bbdee18a6668d9c41cd
aurapusa <apuche@sura.com.co>
2025-12-11 16:44:34 -0500

    Se define el DAG para ingesta de datos de yellow_trips de la TLC, ejecuta una lambda dockerizada en AWS

--- dags/dag_data_processing.py
+++ dags/dag_data_processing.py
@@ -1,14 +1,24 @@
+import json
+import pendulum
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from pendulum import datetime
-from airflow.providers.amazon.aws.operators.lambda_function import AwsLambdaInvokeFunctionOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+
+LOCAL_TZ = "America/Bogota"
 
 with DAG(
+    """Definición del DAG para ingesta de datos del dataset de viajes mensuales
+     de NYC TLC (Taxi & Limousine Comission).
+
+    """
+    dag_id="dag_data_processing",
     schedule="@monthly",
-    start_date=pendulum.datetime(2025,5,1, tz=LOCAL_TZ)
+    start_date=pendulum.datetime(2025,1,1, tz=LOCAL_TZ),
     description="Ingesta mensual de datos del dataset NYC TLC",
-    catchup="False"
-    tags=["tlc","lambda","aws"]
+    catchup=False,
+    tags=["tlc","lambda","aws"],
     max_consecutive_failed_dag_runs=3
 ) as dag:
     
@@ -24,16 +34,20 @@ with DAG(
         "date" : "{{ data_interval_start.strftime('%Y-%m') }}",
         "s3_folder" : "landing",
         "bucket" : "aura-puche-data-lake",
-        "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/{{ dataset }}_{{ data_interval_start.strftime('%Y-%m') }}.parquet"
+        "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{{ data_interval_start.strftime('%Y-%m') }}.parquet"
     }
     lambda_payload = json.dumps(lambda_payload_dict)
 
     #Tasks
-    #Lambda ingestion 
-    dataset_extraction="dataset_extraction"
-    aws_conn_id="aws_connection"
-    region_name="us-east-2"
-    function_name="DockerLambdaAwsStack-DockerFuncF47DA747-V8kJFKF8Jhz6"
-    log_type="Tail"
-    innvocation_type="RequestResponse"
-    payload=lambda_payload 
+    #1. Lambda ingestion 
+    lambda_ingest = LambdaInvokeFunctionOperator(
+        task_id="dataset_extraction",
+        aws_conn_id="aws_connection",
+        region_name="us-east-2",
+        function_name="DockerLambdaAwsStack-DockerFuncF47DA747-V8kJFKF8Jhz6",
+        log_type="Tail",
+        invocation_type="RequestResponse",
+        payload=lambda_payload
+    )
+
+    start >> lambda_ingest >> end
