c48f635ce62c4a46fe741c5677c6bcf15dff4854
Aura Puche <aurapuche@Auras-MacBook-Air.local>
2025-12-12 23:53:33 -0500

    Update. Se incluye payload y tarea de Glue para el procesamiento de archivos desde la capa landing

--- dags/dag_data_processing.py
+++ dags/dag_data_processing.py
@@ -2,6 +2,7 @@ import json
 import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
 from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
@@ -40,9 +41,21 @@ with DAG(
     }
     lambda_payload = json.dumps(lambda_payload_dict)
 
+    glue_raw_to_processed_payload_dict = {
+        "dataset" : dataset,
+        "date" : "{{ data_interval_start.strftime('%Y-%m') }}",
+        "ds" : "{{ data_interval_start.strftime('%Y-%m-%d') }}",
+        "s3_source_folder" : "raw/yellow_tripdata",
+        "s3_target_folder" : "processed/yellow_tripdata",
+        "bucket" : "aura-puche-data-lake",
+        "glue_job_name" : "glue-raw-to-processed-job"
+    }
+    glue_raw_to_processed_payload = json.dumps(glue_raw_to_processed_payload_dict)
+
     #Tasks
+
     lambda_ingest = LambdaInvokeFunctionOperator(
-    """Lambda ingestion for one month
+    """Lambda ingestion for one month, rwites to landing zone in S3
     """
         task_id="dataset_extraction",
         aws_conn_id="aws_connection",
@@ -53,4 +66,22 @@ with DAG(
         payload=lambda_payload
     )
 
-    start >> lambda_ingest >> end
+    glue_landing_to_raw = GlueJobOperator(
+    """Glue job to move data from landing to raw zone in S3
+    """
+        task_id="glue_landing_to_raw",
+        job_name="glue-landing-to-raw-job",
+        script_location="s3://aura-puche-glue-scripts/glue_landing_to_raw.py",
+        aws_conn_id="aws_connection",
+        region_name="us-east-2",
+        job_desc="Glue job to move data from landing to raw zone in S3",
+        wait_for_completion=True,
+        create_job_kwargs={
+            "GlueVersion": "3.0",
+            "NumberOfWorkers": 2,
+            "WorkerType": "G.1X",
+        },
+        script_args={"--payload": glue_raw_to_processed_payload}
+    )    
+
+    start >> lambda_ingest >> glue_landing_to_raw >> end
